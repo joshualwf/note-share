9fd051e5386f62f07ba79147fddd1077fb6b82bd
Joshua <joshualai9922@gmail.com>
2025-02-27 22:19:18 +0800

    remove hidden class

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <div className="hidden flex-col md:flex">
+        <div className="flex-col md:flex">
           <div className="border-b">
             <div className="flex h-16 items-center px-4">
               <img
