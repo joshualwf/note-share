c9e9c845bc7b35cf4dbf78e5a9556cc8f9b436fb
Joshua <joshualai9922@gmail.com>
2025-03-02 19:18:39 +0800

    fix button bg color

--- src/components/FilterSheet.tsx
+++ src/components/FilterSheet.tsx
@@ -89,7 +89,7 @@ function FilterSheet({
                       onClick={() => toggleResourceType(type)}
                       className={
                         resourceTypesFilter.includes(type)
-                          ? "border-primary bg-slate-100"
+                          ? "border-primary bg-accent"
                           : ""
                       }
                     >
