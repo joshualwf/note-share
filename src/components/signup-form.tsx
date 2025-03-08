1c1616189b05ce86bf36b40b5007a801ac4de5fb
Joshua <joshualai9922@gmail.com>
2025-03-08 20:52:39 +0800

    fix authentication card rounding

--- src/components/signup-form.tsx
+++ src/components/signup-form.tsx
@@ -36,7 +36,7 @@ const SignupForm = ({
       <div className="container">
         <div className="flex flex-col gap-4">
           {/* <div className="mx-auto w-full max-w-sm rounded-md p-6 shadow"> */}
-          <Card className="mx-auto w-full max-w-sm rounded-md p-6">
+          <Card className="mx-auto w-full max-w-sm p-6">
             <div className="mb-6 flex flex-col items-center">
               {/* <a href={logo.url}>
                 <img
