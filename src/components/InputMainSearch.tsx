831652575df4791832e1af21732f2aa8b75ae577
Joshua <joshualai9922@gmail.com>
2025-03-22 16:31:40 +0800

    fix search bar

--- src/components/InputMainSearch.tsx
+++ src/components/InputMainSearch.tsx
@@ -19,7 +19,7 @@ const InputMainSearch = React.forwardRef<
         <input
           type={type}
           className={cn(
-            "h-9 w-full border-input text-muted-foreground px-3 py-1 text-base transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none  disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
+            "bg-background h-9 w-full border-input text-muted-foreground px-3 py-1 text-base transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none  disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
           )}
           ref={ref}
           {...props}
