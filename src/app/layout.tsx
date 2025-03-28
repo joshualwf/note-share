0973c7ce500187cfbd6a4ea5d6bb48231023c2ae
Joshua <joshualai9922@gmail.com>
2025-03-28 20:23:07 +0800

    change font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
 import { MainNav } from "@/components/MainNav";
@@ -11,14 +11,10 @@ import { getUserFromCookie } from "@/lib/auth";
 import { UserNav } from "@/components/UserNav";
 import { Toaster } from "@/components/ui/toaster";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -32,7 +28,11 @@ export default async function RootLayout({
   const user = await getUserFromCookie(); // Fetch user from cookies
 
   return (
-    <html lang="en" suppressHydrationWarning className="h-full">
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={`h-full ${inter.className}`}
+    >
       <body className="flex flex-col h-screen">
         <ThemeProvider
           attribute="class"
