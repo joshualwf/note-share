e6ac07bba5ea538c121c9393c24a7eba3a2aabcc
Joshua <joshualai9922@gmail.com>
2025-04-13 21:08:02 +0800

    fix sorting for get comments

--- src/app/api/comments/getComments/[postId]/route.ts
+++ src/app/api/comments/getComments/[postId]/route.ts
@@ -24,6 +24,9 @@ export async function GET(
           include: {
             user: true,
           },
+          orderBy: {
+            createdAt: "asc",
+          },
         },
       },
       orderBy: {
